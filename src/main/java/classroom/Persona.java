5a28410f0b524fd6cab02adb042a1ca742889bd0
Sebastian Valencia Zapata <sevalenciaz@unal.edu.co>
2022-04-04 01:09:19 -0500

    Primer intento

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -8,7 +8,6 @@ public class Persona {
     
     static {
         totalPersonas = 0;
-        cedula = 3;
     }
 
     Persona(long cedula, String nombre) {
@@ -31,6 +30,12 @@ public class Persona {
 
     Persona(String nombre) {
         this.nombre = "";
+        this.cedula = 1;
+        totalPersonas++;
+    }
+    
+    Persona() {
+        this("", 0);
         totalPersonas++;
     }
     
